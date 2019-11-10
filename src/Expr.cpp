a0844daa21ed88c946bea39e9f69f10eb058037a
Matt Watts <matthewwatts@northwesternmutual.com>
2019-11-09 21:57:39 -0600

    Create expression subtypes.

---
+++ src/Expr.cpp
@@ -0,0 +1,36 @@
+#include "Expr.h"
+
+Binary::Binary(Expr left, Token op, Expr right)
+    : left{left}, op{op}, right{right} {}
+
+Assign::Assign(Token name, Expr value)
+    : name{name}, value{value} {}
+
+Call::Call(Expr callee, Token paren, std::vector<Expr> arguments)
+    : callee{callee}, paren{paren}, arguments{arguments} {}
+
+Get::Get(Expr object, Token name)
+    : object{object}, name{name} {}
+
+Grouping::Grouping(Expr expression)
+    : expression{expression} {}
+
+Literal::Literal() {}
+
+Logical::Logical(Expr left, Token op, Expr right)
+    : left{left}, op{op}, right{right} {}
+
+Set::Set(Expr object, Token name, Expr value)
+    : object{object}, name{name}, value{value} {}
+
+Super::Super(Token keyword, Token method)
+    : keyword{keyword}, method{method} {}
+
+This::This(Token keyword)
+    : keyword{keyword} {}
+
+Unary::Unary(Token op, Expr right)
+    : op{op}, right{right} {}
+
+Variable::Variable(Token name)
+    : name{name} {}
---
+++ src/Expr.h
@@ -0,0 +1,106 @@
+#include <vector>
+#include "Token.h"
+
+class Expr
+{
+};
+
+class Binary : public Expr
+{
+  const Expr left;
+  const Token op;
+  const Expr right;
+
+public:
+  Binary(Expr left, Token op, Expr right);
+};
+
+class Assign : public Expr
+{
+  const Token name;
+  const Expr value;
+
+public:
+  Assign(Token name, Expr value);
+};
+
+class Call : public Expr
+{
+  const Expr callee;
+  const Token paren;
+  const std::vector<Expr> arguments;
+
+public:
+  Call(Expr callee, Token paren, std::vector<Expr> arguments);
+};
+
+class Get : public Expr
+{
+  const Expr object;
+  const Token name;
+
+public:
+  Get(Expr object, Token name);
+};
+
+class Grouping : public Expr
+{
+  const Expr expression;
+
+  Grouping(Expr expression);
+};
+
+class Literal : public Expr
+{
+  // todo extend to handle all literal types
+  Literal();
+};
+
+class Logical : public Expr
+{
+  const Expr left;
+  const Token op;
+  const Expr right;
+
+public:
+  Logical(Expr left, Token op, Expr right);
+};
+
+class Set : public Expr
+{
+  const Expr object;
+  const Token name;
+  const Expr value;
+
+  Set(Expr object, Token name, Expr value);
+};
+
+class Super : public Expr
+{
+  const Token keyword;
+  const Token method;
+
+  Super(Token keyword, Token method);
+};
+
+class This : public Expr
+{
+  const Token keyword;
+
+  This(Token keyword);
+};
+
+class Unary : public Expr
+{
+  const Token op;
+  const Expr right;
+
+  Unary(Token op, Expr right);
+};
+
+class Variable : public Expr
+{
+  const Token name;
+
+  Variable(Token name);
+};
